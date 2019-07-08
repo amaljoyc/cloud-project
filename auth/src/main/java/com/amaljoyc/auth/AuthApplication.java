27a7a41e43c4068e0d2daa1cc90bd07ad2fb40d2
amaljoyc <amaljoyc@gmail.com>
2019-07-08 23:11:31 +0200

    use JWT for auth server

--- auth/src/main/java/com/amaljoyc/auth/AuthApplication.java
+++ auth/src/main/java/com/amaljoyc/auth/AuthApplication.java
@@ -3,9 +3,7 @@ package com.amaljoyc.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
-@EnableAuthorizationServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
